d242ca138397c937d66211b937fb9cbc879d9011
jozanza <hello@jsavary.com>
2015-08-21 18:30:53 -0400

    mocha config

---
+++ mocha.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Babel compiler
+require('babel/register')({
+  stage: 0
+});
+
+// Create JSDOM and `document`/`window` globals
+var document = global.document = require('jsdom').jsdom(
+  '<!doctype html><html><head></head><body></body></html>'
+);
+var window = global.window = document.defaultView;
+
+// Attach props from `global` to `window`
+for (var key in window) {
+  if (!window.hasOwnProperty(key)) continue;
+  if (key in global) continue;
+  global[key] = window[key];
+}
