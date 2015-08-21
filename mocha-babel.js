c42821ab002917520683f361f657f08d05079b36
jozanza <hello@jsavary.com>
2015-08-21 15:48:21 -0400

    used var statement for node v0.12 compatibility

--- mocha-babel.js
+++ mocha-babel.js
@@ -6,13 +6,13 @@ require('babel/register')({
 });
 
 // Create JSDOM and `document`/`window` globals
-const document = global.document = require('jsdom').jsdom(
+var document = global.document = require('jsdom').jsdom(
   '<!doctype html><html><head></head><body></body></html>'
 );
-const window = global.window = document.defaultView;
+var window = global.window = document.defaultView;
 
 // Attach props from `global` to `window`
-for (let key in window) {
+for (var key in window) {
   if (!window.hasOwnProperty(key)) continue;
   if (key in global) continue;
   global[key] = window[key];
