a77f17ed12e4d37082adda14ee48b062b838dafe
jozanza <hello@jsavary.com>
2015-08-31 15:27:07 -0400

    updated compiler and re-compiled bundle

--- compiler.js
+++ compiler.js
@@ -1,6 +1,13 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
+
+config.plugins = config.plugins.concat([
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+]);
+
 webpack(config).run(function(err, stats) {
   if (err) console.log(err);
 });
